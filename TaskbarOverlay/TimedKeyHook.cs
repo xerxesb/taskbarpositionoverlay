5b0d55da92d4cd5f3c6d22771aff8dc80ed9efd4
Xerxes Battiwalla <xbattiwalla@cochlear.com>
2009-09-03 18:04:08 +1000

    Initial commit

---
+++ TaskbarOverlay/TimedKeyHook.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Windows.Forms;
+using Gma.UserActivityMonitor;
+using Timer=System.Windows.Threading.DispatcherTimer;
+
+namespace TaskbarOverlay
+{
+    public class TimedKeyHook
+    {
+        public event EventHandler<KeyEventArgs> KeyHeldForDuration;
+        public event EventHandler<KeyEventArgs> KeyReleased;
+
+        private Timer _timer;
+        private bool _keyAlreadyTriggered;
+        private readonly Func<KeyEventArgs, bool> _validateKeyPressCriteria;
+        private KeyEventArgs _keyEventArgs;
+
+        public TimedKeyHook(int delayInMilliseconds, Func<KeyEventArgs, bool> validateKeyPressCriteria)
+        {
+            _validateKeyPressCriteria = validateKeyPressCriteria;
+            _timer = new Timer();
+            _timer.Interval = TimeSpan.FromMilliseconds(delayInMilliseconds);
+            _timer.Tick += (o, e) =>
+                                  {
+                                      _keyAlreadyTriggered = true;
+                                      _timer.Stop();
+                                      _timer.Dispatcher.Invoke((Action<KeyEventArgs>)OnKeyHeldForDuration, _keyEventArgs);
+                                  };
+
+            HookManager.KeyDown += HookManagerKeyDown;
+            HookManager.KeyUp += HookManagerKeyUp;
+        }
+
+        private void OnKeyReleased(KeyEventArgs e)
+        {
+            var temp = KeyReleased;
+            if (temp != null) temp(this, e);
+        }
+
+        private void OnKeyHeldForDuration(KeyEventArgs e)
+        {
+            var temp = KeyHeldForDuration;
+            if (temp != null) temp(this, e);
+        }
+
+        void HookManagerKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_keyAlreadyTriggered && _validateKeyPressCriteria.Invoke(e))
+            {
+                HookManager.KeyDown -= HookManagerKeyDown;
+                _timer.Start();
+                _keyEventArgs = e;
+                e.Handled = true;
+            }
+        }
+
+        private void HookManagerKeyUp(object sender, KeyEventArgs e)
+        {
+            if (_keyAlreadyTriggered)
+            {
+                e.Handled = true;
+                _timer.Dispatcher.Invoke((Action<KeyEventArgs>)OnKeyReleased, e);
+            }
+
+            HookManager.KeyDown += HookManagerKeyDown;
+            _timer.Stop();
+            _keyAlreadyTriggered = false;
+        }
+    }
+}
---
+++ TaskbarOverlay/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace TaskbarOverlay
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private TimedKeyHook _keyHook;
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            _keyHook = new TimedKeyHook(300, e => e.KeyCode == Keys.LWin || e.KeyCode == Keys.RWin);
+            _keyHook.KeyHeldForDuration += (o, e) => Content += "Hi ";
+            _keyHook.KeyReleased += (o, e) => Content += "Ho ";
+
+            Left = Screen.PrimaryScreen.Bounds.Left;
+            Top = Screen.PrimaryScreen.Bounds.Top;
+
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+
+            WindowStyle = WindowStyle.None;
+            AllowsTransparency = true;
+            Opacity = 0;
+            TextBlock
+
+        }
+    }
+}
